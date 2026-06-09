21169d1e73d48c881815eea3374f09dd7fa5b042
Arshika Noor <noorarshika798@gmail.com>
2026-06-09 10:50:25 +0600

    UpdateYouDo3.java

--- Task 2/YouDo3.java
+++ Task 2/YouDo3.java
@@ -1,1 +1,13 @@
-
+class Countdown {
+    public void start() {
+        for (int i = 5; i >= 1; i--)
+        {
+            System.out.println(i);
+        }
+        System.out.println("Go!");}}
+        public class main3{
+            public static void main(String[]args){
+                Countdown number  = new Countdown();
+                number.start();
+            }
+        }
