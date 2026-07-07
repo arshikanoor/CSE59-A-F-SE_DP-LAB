470639f9b13bb70f928e906907fc4f07b89637bb
Arshika Noor <noorarshika798@gmail.com>
2026-07-07 11:46:50 +0600

    UpdateYouDo6.java

--- Task 2/YouDo6.java
+++ Task 2/YouDo6.java
@@ -1,1 +1,30 @@
+class Applet {
 
+    public void launch() {
+        System.out.println("Applet launched");
+    }
+}
+
+
+class GameApplet extends Applet {
+
+    public void play() {
+        System.out.println("Playing game");
+    }
+}
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+     
+        GameApplet game = new GameApplet();
+
+  
+        game.launch();
+
+
+        game.play();
+    }
+}
