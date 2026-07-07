591159a758c14b08f2f067a61e364abe41aa6279
Arshika Noor <noorarshika798@gmail.com>
2026-07-07 11:48:22 +0600

    UpdateYouDo7.java

--- Task 2/YouDo7.java
+++ Task 2/YouDo7.java
@@ -1,1 +1,25 @@
+interface Clickable {
 
+    void onClick();
+}
+
+
+class Button implements Clickable {
+
+    @Override
+    public void onClick() {
+        System.out.println("Button was clicked!");
+    }
+}
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Button button = new Button();
+
+        button.onClick();
+    }
+}
