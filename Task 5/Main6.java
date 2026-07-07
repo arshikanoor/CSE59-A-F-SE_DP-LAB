1115c1fe04de12e378b643cea8838d317b99595e
Arshika Noor <noorarshika798@gmail.com>
2026-07-07 11:42:39 +0600

    UpdateMain6.java

--- Task 5/Main6.java
+++ Task 5/Main6.java
@@ -1,1 +1,60 @@
+interface HttpService {
+    void handleHttpRequest();
+}
 
+
+interface DatabaseManager {
+    void executeSqlStatement();
+}
+
+
+interface ContainerOps {
+    void restartDockerContainer();
+}
+
+
+class WebController implements HttpService {
+
+    @Override
+    public void handleHttpRequest() {
+        System.out.println("Routing traffic to endpoint.");
+    }
+}
+
+
+
+class DatabaseService implements DatabaseManager {
+
+    @Override
+    public void executeSqlStatement() {
+        System.out.println("Executing SQL statement.");
+    }
+}
+
+
+class DockerManager implements ContainerOps {
+
+    @Override
+    public void restartDockerContainer() {
+        System.out.println("Restarting Docker container.");
+    }
+}
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        HttpService webController = new WebController();
+        webController.handleHttpRequest();
+
+
+        DatabaseManager database = new DatabaseService();
+        database.executeSqlStatement();
+
+
+        ContainerOps docker = new DockerManager();
+        docker.restartDockerContainer();
+    }
+}
