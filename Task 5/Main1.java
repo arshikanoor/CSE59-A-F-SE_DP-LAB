bf26295464b75fcbd0be3f04d2522f40e0d2480b
Arshika Noor <noorarshika798@gmail.com>
2026-07-07 11:34:05 +0600

    Implement printer, scanner, and fax interfaces

--- Task 5/Main1.java
+++ Task 5/Main1.java
@@ -1,1 +1,57 @@
+interface Printer {
+    void printDocument();
+}
 
+interface Scanner {
+    void scanDocument();
+}
+
+interface FaxMachine {
+    void faxDocument();
+}
+
+
+class BasicPrinter implements Printer {
+
+    @Override
+    public void printDocument() {
+        System.out.println("Printing...");
+    }
+}
+
+
+
+class MultiFunctionCopier implements Printer, Scanner, FaxMachine {
+
+    @Override
+    public void printDocument() {
+        System.out.println("Printing...");
+    }
+
+    @Override
+    public void scanDocument() {
+        System.out.println("Scanning...");
+    }
+
+    @Override
+    public void faxDocument() {
+        System.out.println("Faxing...");
+    }
+}
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        BasicPrinter basicPrinter = new BasicPrinter();
+        basicPrinter.printDocument();
+
+        System.out.println();
+
+        MultiFunctionCopier copier = new MultiFunctionCopier();
+        copier.printDocument();
+        copier.scanDocument();
+        copier.faxDocument();
+    }
+}
