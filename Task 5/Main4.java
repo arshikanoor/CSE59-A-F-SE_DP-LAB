91cada7af0e8bb03a046d47423b07b5022447968
Arshika Noor <noorarshika798@gmail.com>
2026-07-07 11:39:28 +0600

    UpdateMain4.java
    Added OptimizableModel interface and implementations for ResNet18 and MobileNet with quantization methods.

--- Task 5/Main4.java
+++ Task 5/Main4.java
@@ -1,1 +1,50 @@
+interface OptimizableModel {
+    void quantize();
+}
 
+
+
+class ResNet18 implements OptimizableModel {
+
+    @Override
+    public void quantize() {
+        System.out.println("Applying int8 quantization for ResNet-18.");
+    }
+}
+
+
+
+class MobileNet implements OptimizableModel {
+
+    @Override
+    public void quantize() {
+        System.out.println("Applying dynamic quantization for MobileNet.");
+    }
+}
+
+
+
+class InferenceEngine {
+
+    public void optimizeModel(OptimizableModel model) {
+        model.quantize();
+    }
+}
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        InferenceEngine engine = new InferenceEngine();
+
+        OptimizableModel resnet = new ResNet18();
+        engine.optimizeModel(resnet);
+
+
+  
+        OptimizableModel mobilenet = new MobileNet();
+        engine.optimizeModel(mobilenet);
+    }
+}
