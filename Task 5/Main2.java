8947964bbf577398b3c64b87cc76df6b896293f9
Arshika Noor <noorarshika798@gmail.com>
2026-07-07 11:36:11 +0600

    UpdateMain2.java
    Implemented MessageSender interface with EmailSender and SMSSender classes. Added NotificationService for user alerts using dependency injection.

--- Task 5/Main2.java
+++ Task 5/Main2.java
@@ -1,1 +1,62 @@
 
+interface MessageSender {
+    void sendMessage(String message);
+}
+
+
+class EmailSender implements MessageSender {
+
+    @Override
+    public void sendMessage(String message) {
+        System.out.println("Sending email: " + message);
+    }
+}
+
+
+
+class SMSSender implements MessageSender {
+
+    @Override
+    public void sendMessage(String message) {
+        System.out.println("Sending SMS: " + message);
+    }
+}
+
+
+
+class NotificationService {
+
+    private MessageSender messageSender;
+
+    // Dependency Injection through constructor
+    public NotificationService(MessageSender messageSender) {
+        this.messageSender = messageSender;
+    }
+
+    public void alertUser(String msg) {
+        messageSender.sendMessage(msg);
+    }
+}
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+     
+        MessageSender emailSender = new EmailSender();
+        NotificationService emailNotification =
+                new NotificationService(emailSender);
+
+        emailNotification.alertUser("Your account has been created.");
+
+
+  
+        MessageSender smsSender = new SMSSender();
+        NotificationService smsNotification =
+                new NotificationService(smsSender);
+
+        smsNotification.alertUser("Your OTP is 123456.");
+    }
+}
