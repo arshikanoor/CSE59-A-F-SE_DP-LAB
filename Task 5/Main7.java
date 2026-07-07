35cce1c68792933dded41c872451de984432b492
Arshika Noor <noorarshika798@gmail.com>
2026-07-07 11:44:08 +0600

    UpdateMain7.java

--- Task 5/Main7.java
+++ Task 5/Main7.java
@@ -1,1 +1,66 @@
+interface INetworkProtocol {
+    void broadcastMessage(String msg);
+}
 
+
+
+class BluetoothMeshNetwork implements INetworkProtocol {
+
+    @Override
+    public void broadcastMessage(String msg) {
+        System.out.println("Broadcasting via Bluetooth Mesh: " + msg);
+    }
+}
+
+
+class WiFiDirectNetwork implements INetworkProtocol {
+
+    @Override
+    public void broadcastMessage(String msg) {
+        System.out.println("Broadcasting via WiFi Direct: " + msg);
+    }
+}
+
+
+class DisasterAlertSystem {
+
+    private INetworkProtocol network;
+
+    public DisasterAlertSystem(INetworkProtocol network) {
+        this.network = network;
+    }
+
+    public void triggerAlert(String alertData) {
+        network.broadcastMessage(alertData);
+    }
+}
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        INetworkProtocol bluetoothNetwork =
+                new BluetoothMeshNetwork();
+
+        DisasterAlertSystem bluetoothAlertSystem =
+                new DisasterAlertSystem(bluetoothNetwork);
+
+        bluetoothAlertSystem.triggerAlert(
+                "Earthquake detected! Evacuate immediately."
+        );
+
+
+        INetworkProtocol wifiNetwork =
+                new WiFiDirectNetwork();
+
+        DisasterAlertSystem wifiAlertSystem =
+                new DisasterAlertSystem(wifiNetwork);
+
+        wifiAlertSystem.triggerAlert(
+                "Flood warning! Move to higher ground."
+        );
+    }
+}
